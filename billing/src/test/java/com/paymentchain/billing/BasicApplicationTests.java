22343ef2119e351c625c8c2984c08eaad547f954
willy5986 <willy59861@gmail.com>
2024-10-10 15:10:56 -0600

    prueba_evento

--- billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
+++ billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
@@ -12,7 +12,7 @@ public class BasicApplicationTests {
  
  @Test
  public void contextLoads() {
- 	    String meessage= "default message cambio test devops";
+ 	    String meessage= "default message cprueba";
  	    Assert.assertNotNull(meessage);
  }
  
